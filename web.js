8438ed1a7a9d25ef81ba63965c07fe661ef4c993
Tara Rezvani <tararezvani@Tara-Rezvanis-MacBook-Pro.local>
2014-05-17 15:14:48 -0700

    final verision

--- web.js
+++ web.js
@@ -1,15 +1,24 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var restler = require('restler');
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.all('/', function(request, response) {
+    restler.get('http://reddit.com/.json').on('complete', function(reddit) {
+        var titles = "<Response>";
+        for(var i=0; i<5; i++) {
+            titles += "<Sms>" + reddit.data.children[i].data.title + "</Sms>";
+        }
+        titles += "</Response>";
+        response.send(titles);
+    });
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
